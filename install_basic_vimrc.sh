d47d5fe19653b81128418740db27c427910f59d6
Xu,Shipeng <sxu008@e.ntu.edu.sg>
2013-08-28 23:27:23 +0800

    Change basic
    Signed-off-by: Xu,Shipeng <sxu008@e.ntu.edu.sg>

--- install_basic_vimrc.sh
+++ install_basic_vimrc.sh
@@ -1,3 +1,18 @@
 cd ~/.vim_runtime
 cat ~/.vim_runtime/vimrcs/basic.vim > ~/.vimrc
+echo '
+"<Ctrl-X> -- cut (goto visual mode and cut)
+vmap <C-X> "+x
+"
+""<Ctrl-C> -- copy (goto visual mode and copy)
+vmap <C-C> "+y
+
+"<Ctrl-A> -- copy all
+"imap <C-A> <C-O>gg<C-O>gH<C-O>G<Esc>
+"vmap <C-A> <Esc>gggH<C-O>G<Esc>i
+"
+""<Ctrl-V> -- paste
+map <C-V> "+gP
+'>>~/.vimrc
+
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
