3a0feaa4a03173613f3ba55207ede525f57576c9
Xu,Shipeng <sxu008@e.ntu.edu.sg>
2013-08-29 21:32:51 +0800

    Change vimrc

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -11,19 +11,7 @@ try
 source ~/.vim_runtime/my_configs.vim
 catch
 endtry
-
-"<Ctrl-X> -- cut (goto visual mode and cut)
-vmap <C-X> "+x
-"
-""<Ctrl-C> -- copy (goto visual mode and copy)
-vmap <C-C> "+y
-
-"<Ctrl-A> -- copy all
-"imap <C-A> <C-O>gg<C-O>gH<C-O>G<Esc>
-"vmap <C-A> <Esc>gggH<C-O>G<Esc>i
-"
-""<Ctrl-V> -- paste
-map <C-V> "+gP
+set clipboard=unnamedplus
 ' > ~/.vimrc
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
